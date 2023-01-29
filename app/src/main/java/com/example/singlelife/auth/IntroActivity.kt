9e121b6fed6a801edf3d9d83d6a06421d5e1d3db
dbee <danbee0806@gmail.com>
2023-01-29 14:51:49 +0900

    login, join page UI

--- app/src/main/java/com/example/singlelife/auth/IntroActivity.kt
+++ app/src/main/java/com/example/singlelife/auth/IntroActivity.kt
@@ -1,12 +1,34 @@
 package com.example.singlelife.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import com.example.singlelife.R
+import com.example.singlelife.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
+
+        binding.join.setOnClickListener {
+
+            var intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.login.setOnClickListener {
+
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/singlelife/auth/JoinActivity.kt
@@ -0,0 +1,12 @@
+package com.example.singlelife.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.singlelife.R
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}

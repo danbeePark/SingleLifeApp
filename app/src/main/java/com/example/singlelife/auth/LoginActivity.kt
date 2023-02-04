e8d7ba40954c942fd1fc3058906dd3a3508c0a8b
dbee <danbee0806@gmail.com>
2023-02-04 09:05:16 +0900

    ADD login function

--- app/src/main/java/com/example/singlelife/auth/LoginActivity.kt
+++ app/src/main/java/com/example/singlelife/auth/LoginActivity.kt
@@ -2,11 +2,47 @@ package com.example.singlelife.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.example.singlelife.R
+import com.example.singlelife.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+
+    private var TAG = "LoginActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        auth = Firebase.auth
+
+        binding.loginBtn.setOnClickListener {
+
+            var email = binding.emailArea.text.toString()
+            var password = binding.pwdArea.text.toString()
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        }
+                }
+        }
+
     }
 }

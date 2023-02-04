97487c2190bd8bb5b3ed1bddc835a9edf89aa485
dbee <danbee0806@gmail.com>
2023-02-04 09:52:34 +0900

    ADD notjoin-login function

--- app/src/main/java/com/example/singlelife/auth/IntroActivity.kt
+++ app/src/main/java/com/example/singlelife/auth/IntroActivity.kt
@@ -3,31 +3,61 @@ package com.example.singlelife.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.singlelife.R
 import com.example.singlelife.databinding.ActivityIntroBinding
+import com.example.singlelife.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class IntroActivity : AppCompatActivity() {
-
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding : ActivityIntroBinding
+    private var TAG = "IntroActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
+        auth = Firebase.auth
 
         binding.join.setOnClickListener {
-
             var intent = Intent(this, JoinActivity::class.java)
+            intent.flags= Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
             startActivity(intent)
             finish()
         }
 
         binding.login.setOnClickListener {
-
             var intent = Intent(this, LoginActivity::class.java)
+            intent.flags= Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
             startActivity(intent)
             finish()
+        }
+
+        binding.notJoin.setOnClickListener {
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(baseContext, "익명으로 로그인 하셨습니다.",
+                            Toast.LENGTH_SHORT).show()
+                        val user = auth.currentUser
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInAnonymously:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed."+task.exception,
+                            Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+
+
         }
 
     }
